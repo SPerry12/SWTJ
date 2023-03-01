2b2fe2ed86c71b76cb12c3e7b7c4ad3a57b7b283
Shane Perry <perrysm98@gmail.com>
2023-03-01 13:40:31 -0600

    Created/Updated grid to have heat map that may or may not be used :)

--- SWTJ/Assets/Scenes/GridMap/Scripts/Grid.cs
+++ SWTJ/Assets/Scenes/GridMap/Scripts/Grid.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using CodeMonkey.Utils;
+using System;
 
 public class Grid
 {
+    public const int HEAT_MAP_MAX_VALUE = 100;
+    public const int HEAT_MAP_MIN_VALUE = 0;
+    public event EventHandler<OnGridValueChangedEventArgs> OnGridValueChanged;
+    public class OnGridValueChangedEventArgs : EventArgs {
+        public int x;
+        public int y;
+    }
 
     private int width;
     private int height;
     private float cellSize;
     private Vector3 originPosition;
     private int[,] gridArray;
-    private TextMesh[,] debugTextArray;
+    
     public Grid(int width, int height, float cellSize, Vector3 originPosition){
         
         this.width = width;
@@ -20,23 +28,42 @@ public class Grid
         this.originPosition = originPosition;
 
         gridArray = new int[width, height];
-        debugTextArray = new TextMesh[width, height];
 
-        for (int x = 0; x < gridArray.GetLength(0); x++){
-            for (int y = 0; y < gridArray.GetLength(1); y++){
-                debugTextArray[x, y] = UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f, 20, Color.white, TextAnchor.MiddleCenter);
-                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
-                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+        bool showDebug = false;
+        if (showDebug){
+            TextMesh[,] debugTextArray = new TextMesh[width, height];
+
+            for (int x = 0; x < gridArray.GetLength(0); x++){
+                for (int y = 0; y < gridArray.GetLength(1); y++){
+                    debugTextArray[x, y] = UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f, 20, Color.white, TextAnchor.MiddleCenter);
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                    Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
 
             }
         }
+            Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+            Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
 
-        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
-        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+            OnGridValueChanged += (object sender, OnGridValueChangedEventArgs eventArgs) => {
+                debugTextArray[eventArgs.x, eventArgs.y].text = gridArray[eventArgs.x, eventArgs.y].ToString();
+            };
+        } 
     }
 
+    public int GetWidth(){
+        return width;
+    }
+
+    public int GetHeight(){
+        return height;
+    }
+
+    public float GetCellSize(){
+        return cellSize;
+    }
+   
     // Takes an X and Y and converts it to a world position
-    private Vector3 GetWorldPosition(int x, int y) {
+    public Vector3 GetWorldPosition(int x, int y) {
         return new Vector3(x, y) * cellSize + originPosition;
     }
 
@@ -50,8 +77,8 @@ public class Grid
 
     public void SetValue(int x, int y, int value){
         if (x >= 0 && y >= 0 && x < width && y < height){
-            gridArray[x, y] = value;
-            debugTextArray[x, y].text =  gridArray[x, y].ToString();
+            gridArray[x, y] = Mathf.Clamp(value, HEAT_MAP_MIN_VALUE, HEAT_MAP_MAX_VALUE);
+            if (OnGridValueChanged != null) OnGridValueChanged(this, new OnGridValueChangedEventArgs { x = x, y = y});
         }
         
     }
@@ -60,6 +87,10 @@ public class Grid
         int x, y; 
         GetXY(worldPosition, out x, out y);
         SetValue(x, y, value);
+    }
+
+    public void AddValue(int x, int y, int value){
+        SetValue(x, y, GetValue(x, y) + value);
     }
 
     public int GetValue(int x, int y){
@@ -74,5 +105,32 @@ public class Grid
         int x, y;
         GetXY(worldPosition, out x, out y);
         return GetValue(x, y);
+    }
+
+    public void AddValue(Vector3 worldPosition, int value, int fullValueRange, int totalRange){
+        int lowerValueAmount = Mathf.RoundToInt((float)value / (totalRange - fullValueRange));
+        GetXY(worldPosition, out int originX, out int originY);
+        for(int x = 0; x < totalRange; x++){
+            for(int y = 0; y < totalRange - x; y++){
+                int radius = x + y;
+                int addValueAmount = value;
+                if (radius > fullValueRange){
+                    addValueAmount -= lowerValueAmount * (radius - fullValueRange);
+                }
+                AddValue(originX + x, originY + y, addValueAmount);
+
+                if (x != 0){
+                    AddValue(originX - x, originY + y, addValueAmount);
+                }
+
+                if (y != 0){
+                    AddValue(originX + x, originY - y, addValueAmount);
+                    if (x != 0){
+                        AddValue(originX - x, originY - y, addValueAmount);
+                    }
+                }
+                
+            }
+        }
     }
 }
--- SWTJ/Assets/Scenes/GridMap/Scripts/Testing.cs
+++ SWTJ/Assets/Scenes/GridMap/Scripts/Testing.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 using CodeMonkey.Utils;
 
 public class Testing : MonoBehaviour
-{
+{   
+    [SerializeField] private HeatMapVisual heatMapVisual;
     private Grid grid;
-    
     private void Start()
     {
-        grid = new Grid(4, 2, 10f, new Vector3(0, 0));
+        grid = new Grid(100, 100, 4f, Vector3.zero);
+
+        heatMapVisual.SetGrid(grid);
     }
 
     private void Update(){
         if (Input.GetMouseButtonDown(0)){
-            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+            Vector3 position = UtilsClass.GetMouseWorldPosition();
+            int value = grid.GetValue(position);
+            grid.AddValue(position, 100, 5, 40);
         }
 
-        if (Input.GetMouseButtonDown(1)) {
-            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
-        }
+       
     }
 
 }
